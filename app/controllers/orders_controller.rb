1c8d62305e2fc0308ee2c5517b550104a360760f
Yurika-1016 <cix11103@nitech.jp>
2021-02-02 16:28:09 +0900

    forth commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
-    redirect_to root_path if current_user.id == @item.user_id || !@item.order.nil?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order
   end
 
   def create
